7dce2acd4fae8d1c532799aa87bfafeb50cdd501
ElStron <luisstron45@gmail.com>
2024-09-23 22:16:24 -0600

    Refactor

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,7 +1,7 @@
 export const iconsTags = {
     "github": "akar-icons:github-outline-fill",
     "typescript": "logos:typescript-icon",
-    "javascript": "logos:typescript-icon",
+    "javascript": "openmoji:javascript",
     "php": "skill-icons:php-dark",
     "astro": "vscode-icons:file-type-astro",
     "react": "devicon:react",
