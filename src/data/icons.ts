a7c7234d3aa376a8d888505f744afb309ad445e7
ElStron <luisstron45@gmail.com>
2024-09-23 20:57:42 -0600

    Update !!!

---
+++ src/data/icons.ts
@@ -0,0 +1,12 @@
+export const iconsTags = {
+    "github": "akar-icons:github-outline-fill",
+    "typescript": "logos:typescript-icon",
+    "javascript": "logos:typescript-icon",
+    "php": "skill-icons:php-dark",
+    "astro": "vscode-icons:file-type-astro",
+    "react": "devicon:react",
+    "html": "vscode-icons:file-type-html",
+    "css": "vscode-icons:file-type-css",
+    "express": "simple-icons:express",
+    "nodejs": "ri:nodejs-fill",
+} as const
