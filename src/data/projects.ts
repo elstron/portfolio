c95568fdcf0c2e67027a22c28325f005ce94ad79
ElStron <luisstron45@gmail.com>
2025-02-16 00:54:57 -0600

    Add project data and YouTube lite iframe image

---
+++ src/data/projects.ts
@@ -0,0 +1,27 @@
+export const projects = [
+    {
+        title: "F1 Calendar",
+        body: "F1 Calendar created with Astro, React and Googlesheets",
+        href: "https://f1calendar-xi.vercel.app/",
+        image: "https://f1calendar-xi.vercel.app/F1-Schendule-2024.webp",
+        tecnologies: ["Astro", "React", "Typescript"],
+    },{
+        title: "Cine.is",
+        body: "Cine.is is a web application that allows you to search and filter movies.",
+        href: "#",
+        image:"/cineis.jpg",
+        tecnologies: ["PHP", "HTML", "CSS", "Javascript"],
+    },{
+        title: "Ifortnite",
+        body: "Informational website about the fortnite store, news, leaks and more.",
+        href: "https://ifortnite.pages.dev/",
+        image:"/ifortnite.png",
+        tecnologies: ["Astro", "React", "Express", "Typescript"],
+    },{
+        title: "My Astro component ",
+        body: "Astro component: A lite video player of youtube for astro",
+        href: "https://www.npmjs.com/package/youtube-lite-iframe",
+        image:"/youtube-lite-iframe.png",
+        tecnologies: ["Astro", "Typescript", "CSS"],
+    }   
+]
