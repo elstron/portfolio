42a1a59fb2b0db422ea96faf5c5393b5d24b7e30
ElStron <luisstron45@gmail.com>
2025-02-17 20:50:35 -0600

    Add URL Shortener project and restore Cine.is project in projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,12 +6,6 @@ export const projects = [
         image: "https://f1calendar-xi.vercel.app/F1-Schendule-2024.webp",
         tecnologies: ["Astro", "React", "Typescript"],
     },{
-        title: "Cine.is",
-        body: "Cine.is is a web application that allows you to search and filter movies.",
-        href: "#",
-        image:"/cineis.jpg",
-        tecnologies: ["PHP", "HTML", "CSS", "Javascript"],
-    },{
         title: "Ifortnite",
         body: "Informational website about the fortnite store, news, leaks and more.",
         href: "https://ifortnite.pages.dev/",
@@ -23,5 +17,17 @@ export const projects = [
         href: "https://www.npmjs.com/package/youtube-lite-iframe",
         image:"/youtube-lite-iframe.png",
         tecnologies: ["Astro", "Typescript", "CSS"],
+    },{
+        title: "URL Shortener",
+        body: "URL shortener created with HONO (alternative to express), Typescript, htmx, D1 Cloudflare workers.Shorten links and store in a database with sqlite.",
+        href: "https://link-shortener.stron-dev.workers.dev/",
+        image:"/url-shortener.png",
+        tecnologies: ["Hono", "Typescript", "Cloudflare Workers", "HTMX", "SQLite"],
+    },{
+        title: "Cine.is",
+        body: "Cine.is is a web application that allows you to search and filter movies.",
+        href: "#",
+        image:"/cineis.jpg",
+        tecnologies: ["PHP", "HTML", "CSS", "Javascript"],
     }   
 ]
