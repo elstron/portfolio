a81b1605918b7df4e25066021398a712616d19b1
ElStron <luisstron45@gmail.com>
2025-05-06 15:33:58 -0600

    Reorder projects in projects.ts to maintain consistent structure and improve readability

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,18 +11,19 @@ export const projects = [
         href: "https://ifortnite.pages.dev/",
         image:"/ifortnite.png",
         tecnologies: ["Astro", "React", "Express", "Typescript"],
+    },
+    {
+        title: "URL Shortener",
+        body: "URL shortener created with HONO (alternative to express), Typescript, htmx, D1 Cloudflare workers.Shorten links and store in a database with sqlite.",
+        href: "https://link-shortener.stron-dev.workers.dev/",
+        image:"/url-shortener.png",
+        tecnologies: ["Hono", "Typescript", "Cloudflare Workers", "HTMX", "SQLite"],
     },{
         title: "My Astro component ",
         body: "Astro component: A lite video player of youtube for astro",
         href: "https://www.npmjs.com/package/youtube-lite-iframe",
         image:"/youtube-lite-iframe.png",
         tecnologies: ["Astro", "Typescript", "CSS"],
-    },{
-        title: "URL Shortener",
-        body: "URL shortener created with HONO (alternative to express), Typescript, htmx, D1 Cloudflare workers.Shorten links and store in a database with sqlite.",
-        href: "https://link-shortener.stron-dev.workers.dev/",
-        image:"/url-shortener.png",
-        tecnologies: ["Hono", "Typescript", "Cloudflare Workers", "HTMX", "SQLite"],
     },{
         title: "Cine.is",
         body: "Cine.is is a web application that allows you to search and filter movies.",
