a9104de6c66ab56c06503e88142fc7ad825b0cb4
Stron - Jose Luis B. Leyva <luisstron45@gmail.com>
2025-08-07 01:02:55 -0600

    Add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio.stron.me',
   integrations: [],
   output: 'hybrid',
   adapter: cloudflare(),
