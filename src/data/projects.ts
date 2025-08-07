814d1b1e18e17780e5d0e56653af294002e8e698
Stron - Jose Luis B. Leyva <luisstron45@gmail.com>
2025-08-07 01:38:24 -0600

    Add new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,12 +5,6 @@ export const projects = [
         href: "https://f1calendar-xi.vercel.app/",
         image: "https://f1calendar-xi.vercel.app/F1-Schendule-2024.webp",
         tecnologies: ["Astro", "React", "Typescript"],
-    },{
-        title: "Ifortnite",
-        body: "Informational website about the fortnite store, news, leaks and more.",
-        href: "https://ifortnite.pages.dev/",
-        image:"/ifortnite.png",
-        tecnologies: ["Astro", "React", "Express", "Typescript"],
     },
     {
         title: "URL Shortener",
@@ -18,13 +12,29 @@ export const projects = [
         href: "https://link-shortener.stron-dev.workers.dev/",
         image:"/url-shortener.png",
         tecnologies: ["Hono", "Typescript", "Cloudflare Workers", "HTMX", "SQLite"],
-    },{
-        title: "My Astro component ",
+    },
+    {
+        title: "Ifortnite",
+        body: "Informational website about the fortnite store, news, leaks and more.",
+        href: "https://ifortnite.pages.dev/",
+        image:"/ifortnite.png",
+        tecnologies: ["Astro", "React", "Express", "Typescript"],
+    },
+    {
+        title: "Table of Contents for Astro Build",
+        body: "Integration for Astro that automatically adds a table of contents. Project with thousands of downloads.",
+        href: "https://github.com/elstron/astro-table-of-contents",
+        image: "https://cdn.stron.me/astro-table-of-contents.png",
+        tecnologies: ["Astro", "Typescript", "HTML", "CSS"],
+    },
+    {
+        title: "Astro.build Integration: Lite Iframe",
         body: "Astro component: A lite video player of youtube for astro",
         href: "https://www.npmjs.com/package/youtube-lite-iframe",
         image:"/youtube-lite-iframe.png",
         tecnologies: ["Astro", "Typescript", "CSS"],
-    },{
+    },
+    {
         title: "Cine.is",
         body: "Cine.is is a web application that allows you to search and filter movies.",
         href: "#",
